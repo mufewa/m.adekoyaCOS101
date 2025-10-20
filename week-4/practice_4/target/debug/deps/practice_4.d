3f720469164f98ed8c9aa12b8140a95a12bb7763
Adekoya Oluwamufewa Ayodeji <oluwamufewa.adekoya@pau.edu.ng>
2025-10-20 17:10:04 +0100

    Rust program to determine age elgibility for party entry

---
+++ week-4/practice_4/target/debug/deps/practice_4.d
@@ -0,0 +1,5 @@
+C:\Users\Admin\Documents\m.adekoyaCOS101\week-4\practice_4\target\debug\deps\practice_4.d: src\main.rs
+
+C:\Users\Admin\Documents\m.adekoyaCOS101\week-4\practice_4\target\debug\deps\practice_4.exe: src\main.rs
+
+src\main.rs:
